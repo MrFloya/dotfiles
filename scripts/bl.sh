339fb2c191a32e4c550954b84c63c61b43a869d7
Florian Wilkens <floya@live.de>
2015-01-02 16:11:21 +0100

    Restart from scratch.. :)

--- scripts/bl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-brightness=$1
-
-echo $brightness > /sys/class/backlight/intel_backlight/brightness 
--- scripts/pacbackup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Simple script to keep track of pacman's installed package
-pacman -Qqe | grep -v "$(pacman -Qqm)" > /home/florian/backups/.pacbackup
-
