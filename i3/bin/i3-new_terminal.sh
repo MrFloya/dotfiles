26ccaea521a7c22f27291b748338a5f1c472f165
Florian Wilkens <gh@1wilkens.org>
2017-11-06 14:25:25 +0100

    [i3] Removed obsolete script and changed config to i3-gaps

--- i3/bin/i3-new_terminal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Could use i3-sensible-terminal directly but in this case
-# the sleep timeout has to be adjusted
-
-i3-msg "exec i3-sensible-terminal"
-sleep 0.05
-i3-msg "split h; split v; split h"
